2790011a05088d45b45d51b6adecaa95d1e11855
danshCode <soumadipdas18@yahoo.com>
2021-12-16 17:50:11 +0530

    cdscdscsdcsdcsd

--- competitions/views.py
+++ competitions/views.py
@@ -13,23 +13,30 @@ def showallcompetitions(request):
 
 def registercompetition(request, slug):
     comp = Competition.objects.get(id=slug)
-    check_unique=CompTeam.objects.filter(leader=request.user,event=comp)
+    check_unique = CompTeam.objects.filter(leader=request.user, event=comp)
     if request.method != 'POST' and len(check_unique) > 0:
-        messages.error(request, 'You have already registered your team for this event')
+        messages.error(
+            request, 'You have already registered your team for this event')
         return redirect('showallcompetitions')
     else:
         if request.method == 'POST':
             form = CreateTeamForm(request.user, request.POST)
             if form.is_valid():
-                
-                result = form.save(commit=False)
-                result.event = comp
-                result.leader = request.user
-                result.save()
-                form.save_m2m()
-                messages.success(
-                    request, 'You have registered your team for this event successfully')
-                return redirect('showallcompetitions')
+                if ((len(form.cleaned_data['members'])+1) > comp.max_members or (len(form.cleaned_data['members'])+1) < comp.min_members):
+                    print(len(form.cleaned_data['members']))
+                    print(comp.max_members)
+                    messages.error(request, 'Not a valid range')
+                    return render(request, 'competitions/compreg.html', {'comp': comp, 'form': form})
+                else:
+                    result = form.save(commit=False)
+                    result.event = comp
+                    result.leader = request.user
+                    result.save()
+
+                    form.save_m2m()
+                    messages.success(
+                        request, 'You have registered your team for this event successfully')
+                    return redirect('showallcompetitions')
 
         else:
             form = CreateTeamForm(request.user)
