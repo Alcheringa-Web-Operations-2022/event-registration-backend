38b9e053b7fafa9f67ca76ccad93ac4b3a4f7c34
danshCode <soumadipdas18@yahoo.com>
2021-12-31 15:27:12 +0530

    operational error is comingg

--- teams/urls.py
+++ teams/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import views 
+from . import views
 
 urlpatterns = [
     path('', views.team_members, name='team'),
-    path('member/<id>', views.update_member, name='update-member'),
+    path('member/add', views.add_member, name='add-member'),
+    path('member/update', views.update_member, name='update-member'),
     path('members/remove', views.remove_member, name='remove-member'),
 ]
