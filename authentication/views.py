b74c98da679c7bc64d478614467242011f6414a9
Siddhartha kumar singh <siddharthakumarsingh946@gmail.com>
2021-12-31 15:44:21 +0530

    Reg/Login page fox

--- authentication/views.py
+++ authentication/views.py
@@ -11,7 +11,7 @@ from django.views import View
 from .utils import token_generator
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.utils.encoding import force_bytes, force_text
+from django.utils.encoding import force_bytes
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, get_user_model
